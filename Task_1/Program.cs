efe31e64edfe5bca4e77e2293bdc4b432ee29d58
Dmitriy <dmitriykharitonov333@gmail.com>
2023-12-10 22:49:53 +0300

    Выполнил все 4 задания

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -3,7 +3,7 @@ Console.Clear();
 Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(a);
-if (a % 7 == 0 || a % 23 == 0) {
+if (a % 7 == 0 && a % 23 == 0) {
     Console.WriteLine("Введенное число КРАТНО одновременно и 7 и 23");
 }
 else {
--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,4 +1,17 @@
 ﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
 // причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости,
 // в которой находится эта точка.
-
+Console.Clear();
+Console.Write("Введите координаты точки по оси x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты точки по оси y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write($"(x = {x}, ");
+Console.WriteLine($"y = {y})");
+if (x == 0 && y ==0) Console.WriteLine ("Координаты не должны равняться нулю!");
+else {
+    if (x > 0 && y > 0) Console.WriteLine ("Точка находится в I четверти");
+    else if (x < 0 && y > 0) Console.WriteLine ("Точка находится во II четверти");
+    else if (x < 0 && y < 0) Console.WriteLine ("Точка находится в III четверти");
+    else Console.WriteLine ("Точка находится в VI четверти");
+}
